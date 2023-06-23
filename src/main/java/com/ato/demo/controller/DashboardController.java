84dabb4f8e44ee20e9e1b7364df871f402fbe92b
Jonathan Ato Markin <jona10711@gmail.com>
2023-06-23 09:11:57 +0200

    route created

--- src/main/java/com/ato/demo/controller/DashboardController.java
+++ src/main/java/com/ato/demo/controller/DashboardController.java
@@ -6,11 +6,13 @@ import com.ato.demo.model.Course;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DashboardController {
 
+  @RequestMapping("/course")
   public ResponseEntity<ApiResponse<Course>> getCourse() {
     Course course = new Course("Java", 2, 4.5, "Ranga");
     ApiResponse<Course> response = new ApiResponse<>(course, "200", "success");
